4a2d33860c202ec96bbaeaeee5f54c815c40afc4
AnshikaPand <anshikapandey950@gmail.com>
2025-02-18 16:17:41 +0530

    Final code push

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./layout/Layout";
-import HomePage from "./pages/HomePage";
-import TopRatePage from "./pages/TopRatePage";
-import UpComingPage from "./pages/UpComingPage";
-import MovieDetails from "./components/MovieDetails";
-import SearchBarPage from "./pages/SearchBarPage";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const Layout = lazy(() => import("./layout/Layout"));
+const TopRatePage = lazy(() => import("./pages/TopRatePage"));
+const UpComingPage = lazy(() => import("./pages/UpComingPage"));
+const MovieDetails = lazy(() => import("./components/MovieDetails"));
+const SearchBarPage = lazy(() => import("./pages/SearchBarPage"));
 
 function App() {
-
-
-
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/top-rated" element={<TopRatePage />} />
-        <Route path="/:id" element={<MovieDetails />} />
-        <Route path="/upcoming" element={<UpComingPage />} />
-        <Route path="/search" element={<SearchBarPage />} />
-      </Routes>
-    </Layout>
+    <Suspense fallback={<h1>Loading....</h1>}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/top-rated" element={<TopRatePage />} />
+          <Route path="/:id" element={<MovieDetails />} />
+          <Route path="/upcoming" element={<UpComingPage />} />
+          <Route path="/search" element={<SearchBarPage />} />
+        </Routes>
+      </Layout>
+    </Suspense>
   );
 }
 
